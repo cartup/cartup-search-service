050a088566e9404ba6749677b2b70a1e2c7fde5b
Shyam Kumar <kashyam92@gmail.com>
2023-10-08 21:12:01 +0530

    config

--- src/main/java/com/cartup/search/plp/RASHttpRepoClient.java
+++ src/main/java/com/cartup/search/plp/RASHttpRepoClient.java
@@ -1,15 +1,15 @@
 package com.cartup.search.plp;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -27,18 +27,23 @@ public class RASHttpRepoClient {
         conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
         conn.setRequestProperty("Accept", "application/json");
 
-            try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
-                HttpEntity entity = response.getEntity();
+        // Send post request
+        conn.setDoOutput(true);
+        OutputStream os= new DataOutputStream(conn.getOutputStream());
+        os.write(body.getBytes("UTF-8"));
+        os.close();
 
-                if (entity != null) {
-                    jsonResponse = EntityUtils.toString(entity, "UTF-8");
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        StringBuilder output = new StringBuilder();
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            output = output.append(line);
         }
+        br.close();
 
-        return new JSONObject(jsonResponse);
+        conn.disconnect();
+        JSONObject response = new JSONObject(output.toString());
+		return response;
     }
     
     public static void main(String[] args) throws Exception {
